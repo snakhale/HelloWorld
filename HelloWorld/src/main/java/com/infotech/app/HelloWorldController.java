117f247c77353b651ace8d5d2ea265be8c9a9a78
snakhale <samir.nakhale@gmail.com>
2018-05-01 13:36:07 +0530

    commit hello world

---
+++ HelloWorld/src/main/java/com/infotech/app/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.infotech.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(value="/welcome")
+	public String welcome() {
+		return "Welcome to STS";
+	}
+
+}
