1b5861919f686d73181f21ae2803f9a74344a1da
snakhale <samir.nakhale@gmail.com>
2018-05-01 13:38:04 +0530

    commted again

--- HelloWorld/src/main/java/com/infotech/app/HelloWorldController.java
+++ HelloWorld/src/main/java/com/infotech/app/HelloWorldController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
+	//Commited file
 	@GetMapping(value="/welcome")
 	public String welcome() {
 		return "Welcome to STS";
+		
 	}
 
 }
